1657a2831a27e924257993cbe1b6b65b28d7c909
SirLazubone <a.berezutsky@mail.ru>
2025-02-03 12:55:39 +0300

    testing 2

--- src/main/java/com/course_work/Sports_Menagement_Platform/controller/TestController.java
+++ src/main/java/com/course_work/Sports_Menagement_Platform/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class TestController {
@@ -30,6 +32,12 @@ public class TestController {
     public String test() {
         return "test works!";
     }
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 
 
 }
